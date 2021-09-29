b0bba8af4c2645a2785f7d25888d5bdf56cdf8ab
dwijgohil24 <80956416+dwijgohil24@users.noreply.github.com>
2021-09-29 18:17:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ This file contains database configuration
 */
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'id17586140_root');
-define('DB_PASSWORD', 'SH9T\E$fvCfnNP!9');
-define('DB_NAME', 'id17586140_login_signup');
+define('DB_USERNAME', ' '); //enter your username here
+define('DB_PASSWORD', ' '); //enter your own database password here
+define('DB_NAME', ' '); // enter your own databse name here
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
